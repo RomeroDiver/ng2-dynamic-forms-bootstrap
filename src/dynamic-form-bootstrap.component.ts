5810f5c6c245e4afa74d26c045fe348c7f4a8418
Roman Pylypets <roman.pylypets@opstalent.it>
2016-10-25 09:41:36 +0200

    Added styles to checkbox

---
+++ src/dynamic-form-bootstrap.component.ts
@@ -0,0 +1,33 @@
+import {Component, Input, Output, EventEmitter, ContentChild, TemplateRef} from "@angular/core";
+import {FormGroup} from "@angular/forms";
+import {DynamicFormControlComponent, DynamicFormControlModel} from "@ng2-dynamic-forms/core";
+
+export const DYNAMIC_FORM_UI_BOOTSTRAP = "BOOTSTRAP";
+
+@Component({
+
+    selector: "dynamic-form-bootstrap-control",
+    templateUrl: "./dynamic-form-bootstrap.component.html"
+})
+
+export class DynamicFormBootstrapComponent extends DynamicFormControlComponent {
+
+    @Input() bindId: boolean = true;
+    @Input() controlGroup: FormGroup;
+    @Input() model: DynamicFormControlModel;
+    @Input() nestedTemplate: TemplateRef<any>;
+    @Input() hasSuccessStyles: boolean = false;
+    @Input() hasErrorStyles: boolean = false;
+
+    @Output() blur: EventEmitter<FocusEvent> = new EventEmitter<FocusEvent>();
+
+    @Output() focus: EventEmitter<FocusEvent> = new EventEmitter<FocusEvent>();
+
+    @ContentChild(TemplateRef) customTemplate;
+
+    readonly type: string = DYNAMIC_FORM_UI_BOOTSTRAP;
+
+    constructor() {
+        super();
+    }
+}
